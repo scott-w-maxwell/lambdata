3f12aba478ef8db3409c962c0459f205e08458ad
scottwmwork <scottwmwork@users.noreply.github.com>
2019-09-30 13:25:13 -0700

    changes since lecture

---
+++ lambdata_scottwmwork/__init__.py
@@ -0,0 +1,9 @@
+"""
+lambdata – a collection of data science helper functions
+"""
+name = 'lambdata-scottwmwork'
+import pandas as pd
+import numpy as np
+
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
